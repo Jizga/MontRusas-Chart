91cbb3703d51b0e1b3fce18883429b8fb6305c98
Jizga <jizga93@gmail.com>
2020-12-17 21:02:53 +0100

    Chart de barras hecho

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Color por defecto de las letras de todos los charts
+
+Chart.defaults.global.defaultFontColor = "white";
+
+// Zona de la API
+
 fetch("https://coasters-api.herokuapp.com/country/Spain")
   .then((Response) => Response.json())
   .then((data) => printChart(data));
@@ -12,5 +18,50 @@ function printChart(coasters) {
   let shambala = coasters[2];
   let tarantula = coasters[3];
 
-  
+  speedChart(superman, batman, vertigo, abismo, shambala, tarantula);
+}
+
+function speedChart(superman, batman, vertigo, abismo, shambala, tarantula) {
+  let data = {
+    labels: [
+      "Superman",
+      "Batman",
+      "Vértigo",
+      "Abismo",
+      "Shambala",
+      "Tarántula",
+    ],
+    datasets: [
+      {
+        data: [
+          superman.speed,
+          batman.speed,
+          vertigo.speed,
+          abismo.speed,
+          shambala.speed,
+          tarantula.speed,
+        ],
+        label: "km/h",
+        borderWidth: 1,
+        borderColor: [
+          "rgb(116, 72, 194, 1)",
+          "rgb(33, 192, 215, 1)",
+          "rg(217, 158, 43, 1)",
+          "rgb(205, 58, 129, 1)",
+          "rgb(156, 153, 204, 1)",
+          "rgb(225, 78, 202, 1)",
+        ],
+        backgroundColor: [
+          "rgb(116, 72, 194, .2)",
+          "rgb(33, 192, 215, .2)",
+          "rg(217, 158, 43, .2)",
+          "rgb(205, 58, 129, .2)",
+          "rgb(156, 153, 204, .2)",
+          "rgb(225, 78, 202, .2)",
+        ],
+      },
+    ],
+  };
+
+  new Chart("chart1", { type: "bar", data });
 }
